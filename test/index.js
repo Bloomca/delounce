ec697ef06792d9bdee74be4d44d79f1ac7584aa4
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-10 01:18:13 +0100

    switch to prettier

--- test/index.js
+++ test/index.js
@@ -1,21 +1,21 @@
-import sinon from 'sinon';
-import { atLeast, atMost, sleep, queue, limit, polling } from '../src/index';
+import sinon from "sinon";
+import { atLeast, atMost, sleep, queue, limit, polling } from "../src/index";
 
-test('it should return correct result from value', async () => {
+test("it should return correct result from value", async () => {
   const value = 5;
   const res = await atLeast(100, value);
 
   expect(value).toBe(res);
 });
 
-test('it should return correct result from synchronous function', async () => {
+test("it should return correct result from synchronous function", async () => {
   const value = 10;
   const res = await atLeast(100, () => value);
 
   expect(res).toBe(value);
 });
 
-test('it should return correct result from asynchronous function', async () => {
+test("it should return correct result from asynchronous function", async () => {
   const value = 15;
   const fn = new Promise(res => {
     setTimeout(() => res(value), 100);
@@ -25,7 +25,7 @@ test('it should return correct result from asynchronous function', async () => {
   expect(res).toBe(value);
 });
 
-test('it should resolve fast if promise resolves faster than passed time', async () => {
+test("it should resolve fast if promise resolves faster than passed time", async () => {
   const startTime = Date.now();
   const promise = new Promise(res => {
     setTimeout(res, 50);
@@ -35,7 +35,7 @@ test('it should resolve fast if promise resolves faster than passed time', async
   expect(passedTime < 60).toBe(true);
 });
 
-test('it should resolve after time if fn takes more than passed time', async () => {
+test("it should resolve after time if fn takes more than passed time", async () => {
   const startTime = Date.now();
   const promise = new Promise(res => {
     setTimeout(res, 100);
@@ -45,17 +45,17 @@ test('it should resolve after time if fn takes more than passed time', async () 
   expect(passedTime < 60).toBe(true);
 });
 
-test('it should queue fns in correct order', async () => {
+test("it should queue fns in correct order", async () => {
   const spy1 = sinon.spy();
   const spy2 = sinon.spy();
 
-  queue('test', spy1);
-  queue('test', spy2).then(() => {
+  queue("test", spy1);
+  queue("test", spy2).then(() => {
     sinon.assert.callOrder(spy1, spy2);
   });
 });
 
-test('it should sleep given amount of time', async () => {
+test("it should sleep given amount of time", async () => {
   const startTime = Date.now();
 
   await sleep(100);
@@ -64,7 +64,7 @@ test('it should sleep given amount of time', async () => {
   expect(difference > 90 && difference < 110).toBe(true);
 });
 
-test('it should limit minimum time, if it executes faster', async () => {
+test("it should limit minimum time, if it executes faster", async () => {
   const fn = new Promise(res => setTimeout(res, 100));
 
   const startTime = Date.now();
@@ -74,7 +74,7 @@ test('it should limit minimum time, if it executes faster', async () => {
   expect(difference < 160 && difference > 140).toBe(true);
 });
 
-test('it should limit maximum time, if it executes longer', async () => {
+test("it should limit maximum time, if it executes longer", async () => {
   const fn = new Promise(res => setTimeout(res, 400));
 
   const startTime = Date.now();
@@ -84,7 +84,7 @@ test('it should limit maximum time, if it executes longer', async () => {
   expect(difference < 310 && difference > 290).toBe(true);
 });
 
-test('it should not limit time, if it executes in range', async () => {
+test("it should not limit time, if it executes in range", async () => {
   const fn = new Promise(res => setTimeout(res, 225));
 
   const startTime = Date.now();
@@ -94,7 +94,7 @@ test('it should not limit time, if it executes in range', async () => {
   expect(difference < 235 && difference > 215).toBe(true);
 });
 
-test('polling should invoke until fn returns true', async () => {
+test("polling should invoke until fn returns true", async () => {
   let i = 0;
   const poll = () => {
     i++;
@@ -108,7 +108,7 @@ test('polling should invoke until fn returns true', async () => {
   expect(i).toBe(3);
 });
 
-test('polling should not invoke fn in case of cancelling', async () => {
+test("polling should not invoke fn in case of cancelling", async () => {
   let i = 0;
   const poll = () => {
     i++;
@@ -124,7 +124,7 @@ test('polling should not invoke fn in case of cancelling', async () => {
   expect(i).toBe(1);
 });
 
-test('polling should accept async poll function', async () => {
+test("polling should accept async poll function", async () => {
   let i = 0;
   const poll = async () => {
     await sleep(10);
